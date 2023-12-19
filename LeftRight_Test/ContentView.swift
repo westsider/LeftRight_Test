980cfae8fbda38ab4a416d6411dff0ac5ff2008a
westsider <whansen1@mac.com>
2023-12-19 08:54:35 -0600

    Initial Commit

---
+++ LeftRight_Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LeftRight_Test
+//
+//  Created by Warren Hansen on 12/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LeftRight_Test/LeftRight_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeftRight_TestApp.swift
+//  LeftRight_Test
+//
+//  Created by Warren Hansen on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LeftRight_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
